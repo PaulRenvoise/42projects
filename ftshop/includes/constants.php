0ebccde1e6061ddb100e181653a41b4557332a46
Paul Renvoise <renvoisepaul@gmail.com>
2015-02-02 14:18:13 +0100

    [Add] raytracer branch

--- ftshop/includes/constants.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-define('BASE_URL', getenv('HTTP_HOST') . '/');
-define('DB_HOST', 'localhost');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', 'toor');
-define('DB_NAME', 'rushop');
-
-?>
--- intra/src/Site/PlanningBundle/SitePlanningBundle.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Site\PlanningBundle;
-
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-class SitePlanningBundle extends Bundle
-{
-}
